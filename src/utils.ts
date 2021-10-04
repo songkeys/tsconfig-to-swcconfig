056549335b6a9a5388b3f8ca4b1c0e12cd4f61af
Songkeys <songv587@gmail.com>
2021-10-04 16:10:54 +0800

    feat: support extends in tsconfig

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import JoyCon from 'joycon'
 import type * as ts from 'typescript'
 import { parse } from 'jsonc-parser'
+import deepmerge from 'deepmerge'
 
 const joycon = new JoyCon()
 
@@ -17,9 +18,24 @@ export function getTSOptions(
   filename: string = 'tsconfig.json',
   cwd: string = process.cwd(),
 ): ts.CompilerOptions | null {
-  const { data, path } = joycon.loadSync([filename], cwd)
+  const { compilerOptions } = loadTsFile(filename, cwd) ?? {}
+  return compilerOptions
+}
+
+function loadTsFile(filename: string, cwd: string, tsConfig?: any): any {
+  let { data, path } = joycon.loadSync([filename], cwd)
   if (path && data) {
-    return data.compilerOptions
+    if (tsConfig) {
+      data = deepmerge(tsConfig, data)
+    }
+    const { extends: _extends } = data
+    if (_extends) {
+      delete data.extends
+      return loadTsFile(path, cwd, data)
+    } else {
+      return data
+    }
+  } else {
+    return null
   }
-  return null
 }
--- test/utils.ts
+++ test/utils.ts
@@ -8,9 +8,18 @@ test('read tsconfig file', (t) => {
     path.resolve(__dirname, 'fixtures', 'tsconfig'),
   )
   t.match(result, { target: 'esnext' })
+
   result = getTSOptions()
   t.match(result, { target: 'es2018' })
+
   result = getTSOptions('tsconfig.json', path.resolve('/')) // a place with no tsconfig
   t.match(result, null)
+
+  result = getTSOptions(
+    'tsconfig-extends.json',
+    path.resolve(__dirname, 'fixtures', 'tsconfig'),
+  )
+  t.match(result, { target: 'es2018' })
+
   t.end()
 })
