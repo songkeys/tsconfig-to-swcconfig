d2bf8c4d5561424d345334e9021a694007af8b82
IhsanMujdeci <ihsan@experience.digital>
2023-03-09 20:58:54 +1100

    feat: allow imported extends (#12)
    Co-authored-by: Songkeys <songv587@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import JoyCon from 'joycon'
-import type * as ts from 'typescript'
+import type ts from 'typescript'
 import { parse } from 'jsonc-parser'
 import deepmerge from 'deepmerge'
 
@@ -23,19 +23,42 @@ export function getTSOptions(
 }
 
 function loadTsFile(filename: string, cwd: string, tsConfig?: any): any {
-  let { data, path } = joycon.loadSync([filename], cwd)
+  let { data, path } = resolveFile(filename, cwd) ?? {}
+
   if (path && data) {
     if (tsConfig) {
-      data = deepmerge(tsConfig, data)
+      data = deepmerge(data, tsConfig)
     }
-    const { extends: _extends } = data
+    let { extends: _extends } = data
     if (_extends) {
       delete data.extends
-      return loadTsFile(path, cwd, data)
+      if (!_extends.endsWith('.json')) {
+        _extends += '.json'
+      }
+      return loadTsFile(_extends, cwd, data)
     } else {
       return data
     }
   } else {
+    return tsConfig
+  }
+}
+
+function resolveFile(
+  filename: string,
+  cwd: string,
+): {
+  data: any
+  path: string
+} | null {
+  try {
+    let { data, path } = joycon.loadSync([filename], cwd)
+    if (!path || !data) {
+      data = require(filename)
+      path = require.resolve(filename, { paths: [cwd] })
+    }
+    return { data, path }
+  } catch (e) {
     return null
   }
 }
--- test/utils.ts
+++ test/utils.ts
@@ -19,7 +19,22 @@ test('read tsconfig file', (t) => {
     'tsconfig-extends.json',
     path.resolve(__dirname, 'fixtures', 'tsconfig'),
   )
-  t.match(result, { target: 'es2018' })
+  t.match(result, { target: 'es2018', strict: true })
+
+  result = getTSOptions(
+    'tsconfig-extends-imported.json',
+    path.resolve(__dirname, 'fixtures', 'tsconfig'),
+  )
+  t.match(result, {
+    lib: ['es2019', 'es2020.promise', 'es2020.bigint', 'es2020.string'],
+    module: 'commonjs',
+    target: 'es2018',
+    strict: true,
+    esModuleInterop: true,
+    skipLibCheck: true,
+    forceConsistentCasingInFileNames: true,
+    moduleResolution: 'node',
+  })
 
   t.end()
 })
